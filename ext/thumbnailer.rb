a18200da2be7ce0603e214e1ebabdd51885a11c0
Matteo Collina <matteo.collina@gmail.com>
2009-09-07 12:59:16 +0200

    Dropped Thumbnailer configurability in favor of meta informations.
    From now it's possibile to configure all .image files properties only
    with the use of metainfo files.

--- ext/thumbnailer.rb
+++ ext/thumbnailer.rb
@@ -4,22 +4,13 @@ config = Webgen::WebsiteAccess.website.config
 config.patterns('Thumbnailer' => ['**/*.image'])
 config['sourcehandler.invoke'][5] << 'Thumbnailer'
 config['sourcehandler.default_meta_info']['Thumbnailer'] = {
-  "image.width" => nil,
-  "image.height" => nil,
-  "thumb.width" => nil,
-  "thumb.height" => nil,
+  "image.width" => 500,
+  "image.height" => 500,
+  "thumb.width" => 75,
+  "thumb.height" => 75,
   "resize" => true,
   "thumb" => true
 }
-
-config.thumbnailer.image.width(500, :doc => 'The max width of the processed image.')
-config.thumbnailer.image.height(500, :doc => 'The max height of the processed image.')
-
-config.thumbnailer.thumb.width(75, :doc => 'The max width of the processed thumbnail.')
-config.thumbnailer.thumb.height(75, :doc => 'The max height of the processed thumbnail.')
-
-config.thumbnailer.resize(true, :doc => 'The thumbnailer resize every image by default, but you can disable it.')
-config.thumbnailer.thumb(true, :doc => 'The thumbnailer thumb every image by default, but you can disable it.')
 
 unless Object.const_defined?(:Thumbnailer)
   require 'RMagick'
@@ -32,42 +23,46 @@ unless Object.const_defined?(:Thumbnailer)
 
     def create_node(path)
       nodes = []
-      
+
       dest_path = Path.new(path.path.gsub(/\.image/,''), path.source_path)
       dest_path.meta_info.update(path.meta_info)
 
-      if info(dest_path,"resize")
-        nodes << website.blackboard.invoke(:create_nodes, dest_path, self) do |cn_path|
-          update_node(super(cn_path),
-            info(cn_path,"image.width"),
-            info(cn_path,"image.height"))
-        end
+      nodes << website.blackboard.invoke(:create_nodes, dest_path, self) do |cn_path|
+        super(cn_path)
+      end
+      nodes.flatten!
+      main_node = nodes.first
+
+      if main_node["resize"]
+        update_node(main_node,
+          main_node["image.width"],
+          main_node["image.height"])
       end
 
       thumb_path = Path.new(dest_path.path.gsub(dest_path.ext, "thumb." + dest_path.ext), path.source_path)
       thumb_path.meta_info.update(path.meta_info)
 
-      if info(dest_path,"thumb")
+      if main_node["thumb"]
         nodes << website.blackboard.invoke(:create_nodes, thumb_path, self) do |cn_path|
           update_node(super(cn_path),
-            info(cn_path,"thumb.width"),
-            info(cn_path,"thumb.height"))
+            main_node["thumb.width"],
+            main_node["thumb.height"])
         end
       end
 
-      nodes.flatten
+      nodes.flatten!
     end
 
     def content(node)
       info = node.node_info
 
-      unless info.has_key? :image_width and info.has_key? :image_height
-        raise "Missing :image_width and :image_height"
-      end
-
       io = website.blackboard.invoke(:source_paths)[info[:src]].io
       image = Magick::Image.from_blob(io.data).first
-      image.resize_to_fit!(info[:image_width], info[:image_height])
+
+      if info.has_key? :image_width and info.has_key? :image_height
+        image.resize_to_fit!(info[:image_width], info[:image_height])
+      end
+      
       Path::SourceIO.new { StringIO.new(image.to_blob) }
     end
 
@@ -78,10 +73,5 @@ unless Object.const_defined?(:Thumbnailer)
       node.node_info[:image_height] = height
       node
     end
-
-    def info(node, key)
-      node.meta_info[key] ? node.meta_info[key] : website.config["thumbnailer." + key]
-    end
-
   end
 end
