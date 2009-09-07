6b5639238a512d806f4747d658c13d8d4484b015
Matteo Collina <matteo.collina@gmail.com>
2009-09-07 11:40:19 +0200

    Refactored Thumbnailer to use the meta informations.
    The Thumbnailer can now get all the configuration params that it needs
    from the meta_info of the node/path but if there aren't it can fetch
    them from website configuration.

--- ext/thumbnailer.rb
+++ ext/thumbnailer.rb
@@ -3,88 +3,27 @@ config = Webgen::WebsiteAccess.website.config
 
 config.patterns('Thumbnailer' => ['**/*.image'])
 config['sourcehandler.invoke'][5] << 'Thumbnailer'
+config['sourcehandler.default_meta_info']['Thumbnailer'] = {
+  "image.width" => nil,
+  "image.height" => nil,
+  "thumb.width" => nil,
+  "thumb.height" => nil,
+  "resize" => true,
+  "thumb" => true
+}
 
 config.thumbnailer.image.width(500, :doc => 'The max width of the processed image.')
 config.thumbnailer.image.height(500, :doc => 'The max height of the processed image.')
 
-config.thumbnailer.thumbnail.width(75, :doc => 'The max width of the processed thumbnail.')
-config.thumbnailer.thumbnail.height(75, :doc => 'The max height of the processed thumbnail.')
+config.thumbnailer.thumb.width(75, :doc => 'The max width of the processed thumbnail.')
+config.thumbnailer.thumb.height(75, :doc => 'The max height of the processed thumbnail.')
 
+config.thumbnailer.resize(true, :doc => 'The thumbnailer resize every image by default, but you can disable it.')
+config.thumbnailer.thumb(true, :doc => 'The thumbnailer thumb every image by default, but you can disable it.')
 
 unless Object.const_defined?(:Thumbnailer)
   require 'RMagick'
   require 'stringio'
-  
-  #  class Thumbnailer
-  #
-  #    include Webgen
-  #    include Tag::Base
-  #
-  #    def initialize
-  #      @source_handler = ImageSourceHandler.new
-  #    end
-  #
-  #    def call(tag, body, context)
-  #      self.send(tag.to_sym,context)
-  #    end
-  #
-  #    def image(context)
-  #      source_node = find_image_node(context)
-  #      dest_path = Path.new(source_node.path)
-  #      #dest_path = source_node.path
-  #      source_node.tree.delete_node(source_node)
-  #      result_node = @source_handler.create_node(dest_path,
-  #        param('tag.thumbnailer.image.width'),
-  #        param('tag.thumbnailer.image.height')
-  #      )
-  #      build_img_tag(result_node)
-  #    end
-  #
-  #    def linktoimage(context)
-  #      raise NotImplementedError.new
-  #    end
-  #
-  #    def thumbnail(context)
-  #      source_node = find_image_node(context)
-  #
-  #      match = /(\.[a-z]+)$/.match(source_node.path)
-  #      dest_path = Path.new(source_node.path.gsub(match[1], ".thumb" + match[1]))
-  #      dest_node = @source_handler.create_node(dest_path,
-  #        param('tag.thumbnailer.thumbnail.width'),
-  #        param('tag.thumbnailer.thumbnail.height')
-  #      )
-  #      build_img_tag(dest_node)
-  #    end
-  #
-  #    private
-  #    def tag_config_base
-  #      "tag.thumbnailer"
-  #    end
-  #
-  #    def find_image_node(context)
-  #      image_name = param("tag.thumbnailer.image.source")
-  #
-  #      if context.website.tree.node_access[:path].has_key? image_name
-  #        source_node = context.website.tree.node_access[:path][image_name]
-  #      elsif context.website.tree.node_access[:path].has_key? File.join(context.node.parent.path, image_name)
-  #        source_node = context.website.tree.node_access[:path][File.join(context.node.parent.path, image_name)]
-  #      else
-  #        potential_nodes = context.website.tree.node_access[:path].values.select do |node|
-  #          puts node.path
-  #          Webgen::Path.match(node.path,"**/#{image_name}") or node.path == image_name
-  #        end
-  #        puts potential_nodes.size
-  #        source_node = potential_nodes.first
-  #      end
-  #
-  #      raise "No image found: #{image_name}" if source_node.nil?
-  #      source_node
-  #    end
-  #
-  #    def build_img_tag(node)
-  #      "<img src=\'#{node.path}\'/>"
-  #    end
-  #  end
 
   class Thumbnailer
     include Webgen
@@ -97,19 +36,23 @@ unless Object.const_defined?(:Thumbnailer)
       dest_path = Path.new(path.path.gsub(/\.image/,''), path.source_path)
       dest_path.meta_info.update(path.meta_info)
 
-      nodes << website.blackboard.invoke(:create_nodes, dest_path, self) do |cn_path|
-        update_node(super(cn_path),
-          website.config["thumbnailer.image.width"],
-          website.config["thumbnailer.image.height"])
+      if info(dest_path,"resize")
+        nodes << website.blackboard.invoke(:create_nodes, dest_path, self) do |cn_path|
+          update_node(super(cn_path),
+            info(cn_path,"image.width"),
+            info(cn_path,"image.height"))
+        end
       end
 
       thumb_path = Path.new(dest_path.path.gsub(dest_path.ext, "thumb." + dest_path.ext), path.source_path)
       thumb_path.meta_info.update(path.meta_info)
-      
-      nodes << website.blackboard.invoke(:create_nodes, thumb_path, self) do |cn_path|
-        update_node(super(cn_path),
-          website.config["thumbnailer.thumbnail.width"],
-          website.config["thumbnailer.thumbnail.height"])
+
+      if info(dest_path,"thumb")
+        nodes << website.blackboard.invoke(:create_nodes, thumb_path, self) do |cn_path|
+          update_node(super(cn_path),
+            info(cn_path,"thumb.width"),
+            info(cn_path,"thumb.height"))
+        end
       end
 
       nodes.flatten
@@ -134,6 +77,10 @@ unless Object.const_defined?(:Thumbnailer)
       node.node_info[:image_width] = width
       node.node_info[:image_height] = height
       node
+    end
+
+    def info(node, key)
+      node.meta_info[key] ? node.meta_info[key] : website.config["thumbnailer." + key]
     end
 
   end
