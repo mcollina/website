63c7a763e7339a764ee755e2e5448cff92e6099b
Matteo Collina <matteo.collina@gmail.com>
2009-09-21 09:55:28 +0200

    Updated twitterbox due to the changes in the twitter name.

--- src/js/twitterbox.js
+++ src/js/twitterbox.js
@@ -38,4 +38,4 @@ function relative_time(time_value) {
   }
 }
 
-loadJs("http://twitter.com/statuses/user_timeline/matteo_collina.json?callback=twitterCallback&count=5")
+loadJs("http://twitter.com/statuses/user_timeline/matteocollina.json?callback=twitterCallback&count=5")
