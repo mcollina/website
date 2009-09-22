0fe17b20e0c2fbaa10cc290257bf9795b0ed81cc
Matteo Collina <matteo.collina@gmail.com>
2009-09-22 13:07:09 +0200

    Refactored I18nTag class to allow string interpolation.

--- ext/i18n_tag.rb
+++ ext/i18n_tag.rb
@@ -11,24 +11,44 @@ module I18n
   end
 end
 
+# This class provides the tag +t+ which allows a look-up based translation like
+# rails i18n. If <tt>{t: a_key}</tt> is provided an string identified
+# by <tt>:a_key</tt> will be looked up in the translation files.
+# This tag supports object interpolation like rails does, i.e.
+# <tt>{t: {key => a_key, object => the_object}}</tt> will search for a string
+# identified by <tt>:a_key</tt> and all occurrences of <tt>{{object}}</tt> in
+# such string will be replaced by <tt>the_object</tt>.
 class I18nTag
 
   include Webgen
   include Tag::Base
 
+  KEY = "key"
+
+  attr_reader :params
+
   def self.setup()
     WebsiteAccess.website.config.data["contentprocessor.tags.map"]['t'] = 'I18nTag'
-    WebsiteAccess.website.config.tag.i18n.key(nil, :doc => 'The key to translate', :mandatory => 'default')
   end
 
   def call(tag, body, context)
-    key = param('tag.i18n.key')
-    I18n.translate(key, :locale => context.node.lang)
+    translation_params = {}
+    params.each do |key, value|
+      translation_params[key.to_sym] = value
+    end
+    key = translation_params.delete(KEY.to_sym)
+    
+    translation_params[:locale] = context.node.lang
+    I18n.translate(key, translation_params)
   end
 
-  private
+  def create_params_hash(config, node)
+    if config.kind_of? Hash
+      result = config
+    else
+      result = {KEY => config}
+    end
 
-  def tag_config_base
-    "tag.i18n"
+    result
   end
 end
