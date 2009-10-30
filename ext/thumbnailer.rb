732c7031907fa23e1b63672b261aeb152380ae8a
Matteo Collina <matteo@matteo-laptop.(none)>
2009-10-30 14:17:53 +0100

    Added nice polaroid effect in the blog's images.

--- ext/thumbnailer.rb
+++ ext/thumbnailer.rb
@@ -14,12 +14,14 @@ class Thumbnailer
     
     config['sourcehandler.invoke'][5] << 'Thumbnailer'
     config['sourcehandler.default_meta_info']['Thumbnailer'] = {
-      "image.width" => 500,
-      "image.height" => 500,
+      "image.width" => 300,
+      "image.height" => 300,
       "thumb.width" => 100,
       "thumb.height" => 100,
       "resize" => true,
-      "thumb" => true
+      "thumb" => true,
+      "image.polaroid" => false,
+      "thumb.polaroid" => false
     }
   end
 
@@ -38,7 +40,8 @@ class Thumbnailer
     if main_node["resize"]
       update_node(main_node,
         main_node["image.width"],
-        main_node["image.height"])
+        main_node["image.height"],
+        main_node["image.polaroid"])
     end
 
     thumb_path = Path.new(dest_path.path.gsub(dest_path.ext, "thumb." + dest_path.ext), path.source_path)
@@ -48,7 +51,8 @@ class Thumbnailer
       nodes << website.blackboard.invoke(:create_nodes, thumb_path, self) do |cn_path|
         update_node(super(cn_path),
           main_node["thumb.width"],
-          main_node["thumb.height"])
+          main_node["thumb.height"],
+          main_node["thumb.polaroid"])
       end
     end
 
@@ -64,15 +68,24 @@ class Thumbnailer
     if info.has_key? :image_width and info.has_key? :image_height
       image.resize_to_fit!(info[:image_width], info[:image_height])
     end
+
+    if info[:polaroid]
+      image.background_color = "none"
+      image.format = "PNG"
+      image = image.polaroid do
+        self.fill = "white"
+      end
+    end
       
     Path::SourceIO.new { StringIO.new(image.to_blob) }
   end
 
   private
 
-  def update_node(node, width, height)
+  def update_node(node, width, height, polaroid)
     node.node_info[:image_width] = width
     node.node_info[:image_height] = height
+    node.node_info[:polaroid] = polaroid
     node
   end
 end
