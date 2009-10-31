8beb86ab0899b805686adaaf2ef9e13c709a24ed
Matteo Collina <matteo.collina@gmail.com>
2009-10-31 14:49:49 +0100

    Little refactoring in the blog engine.

--- ext/blog.rb
+++ ext/blog.rb
@@ -36,7 +36,6 @@ class Blog
       raise "The meta_info #{POST_PER_PAGE} should be greater than one for path: #{path.path}"
     end
 
-
     # select only blog's posts
     posts = website.tree.node_access[:path].values.select do |n|
       Webgen::Path.match(n.path,meta_info[FILTER])
@@ -44,48 +43,7 @@ class Blog
 
     raise "No node selected by the key: #{FILTER}" if posts.size == 0
 
-    # order them by their creation date, newer posts first
-    posts.sort! do |a,b|
-      a.meta_info["created_at"] <=> b.meta_info["created_at"]
-    end
-
-    # divide posts in pages, starting from the oldest posts
-    posts_pages = [[]]
-    posts.each do |post|
-      posts_pages << [] if posts_pages.last.size >= meta_info[POST_PER_PAGE]
-      posts_pages.last.unshift post
-    end
-
-    # generates nodes
-    created_nodes = []
-    page_sourcehandler = SourceHandler::Page.new
-    posts_pages.each_index do |index|
-      dest_path = Path.new(path.source_path.gsub(/blog$/, "#{index + 1}.html"), path.source_path) do
-        StringIO.new(path.io.data)
-      end
-      created_nodes << website.blackboard.invoke(:create_nodes, dest_path, page_sourcehandler)
-    end
-    created_nodes.flatten!
-
-    # fill every node with its meta informations
-    # the lower the index, the oldest the articles
-    # the articles are displaied starting from the latest, so the next node
-    # as an index lower than the current node, while the previous node is the
-    # element with index greater than the current node.
-    created_nodes.each_with_index do |node, index|
-      next_node = (index > 0) ? created_nodes[index-1] : nil
-      prev_node = (index < created_nodes.size - 1) ? created_nodes[index+1] : nil
-      node[POSTS_ITERATOR] = PostsIterator.new(posts_pages[index],prev_node,next_node)
-      node['in_menu'] = node['in_menu'] && prev_node.nil?
-        
-      [:used_meta_info_nodes, :used_nodes].each do |key|
-        node[key] ||= []
-        node[key] = node[key].concat(posts_pages[index])
-      end
-
-      node.node_info[:created_by_blog] = true
-    end
-    created_nodes
+    create_blog_nodes(path, posts, meta_info[POST_PER_PAGE])
   end
 
   def content(node)
@@ -118,6 +76,63 @@ class Blog
       node.flag(:dirty) unless path && !path.changed?
     end
 
+  end
+
+  def create_translated_node(source_node, lang)
+    return source_node if source_node.in_lang(lang)
+
+    dest_path = Webgen::Path.lcn(source_node.path, lang)
+    dest_info = source_node.meta_info.dup
+    dest_info["lang"] = lang
+
+    new_node = Webgen::Node.new(source_node.parent, dest_path, source_node.cn, dest_info)
+    new_node.node_info.merge!(source_node.node_info)
+    new_node
+  end
+
+  def create_blog_nodes(path, posts, posts_per_page)
+    # order them by their creation date, newer posts first
+    posts.sort! do |a,b|
+      a.meta_info["created_at"] <=> b.meta_info["created_at"]
+    end
+
+    # divide posts in pages, starting from the oldest posts
+    posts_pages = [[]]
+    posts.each do |post|
+      posts_pages << [] if posts_pages.last.size >= posts_per_page
+      posts_pages.last.unshift post
+    end
+
+    # generates nodes
+    created_nodes = []
+    page_sourcehandler = SourceHandler::Page.new
+    posts_pages.each_index do |index|
+      dest_path = Path.new(path.source_path.gsub(/blog$/, "#{index + 1}.html"), path.source_path) do
+        StringIO.new(path.io.data)
+      end
+      created_nodes << website.blackboard.invoke(:create_nodes, dest_path, page_sourcehandler)
+    end
+    created_nodes.flatten!
+
+    # fill every node with its meta informations
+    # the lower the index, the oldest the articles
+    # the articles are displaied starting from the latest, so the next node
+    # as an index lower than the current node, while the previous node is the
+    # element with index greater than the current node.
+    created_nodes.each_with_index do |node, index|
+      next_node = (index > 0) ? created_nodes[index-1] : nil
+      prev_node = (index < created_nodes.size - 1) ? created_nodes[index+1] : nil
+      node[POSTS_ITERATOR] = PostsIterator.new(posts_pages[index],prev_node,next_node)
+      node['in_menu'] = node['in_menu'] && prev_node.nil?
+
+      [:used_meta_info_nodes, :used_nodes].each do |key|
+        node[key] ||= []
+        node[key] = node[key].concat(posts_pages[index])
+      end
+
+      node.node_info[:created_by_blog] = true
+    end
+    created_nodes
   end
 end
 
